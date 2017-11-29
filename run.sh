39d78ab2c4c9c1236aae910011117b26e85142c6
Goran Cvijanovic <goran.rs.bg@gmail.com>
2017-11-29 19:07:53 +0100

    only payara can run this war without errors...

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-NAME=payara
+NAME=payara-ping
 docker kill ${NAME}
 docker rm ${NAME}
-docker run -d -p 8080:8080 -p 4848:4848 --name ${NAME}  airhacks/payara
+docker run -d -p 8080:8080 -p 4848:4848 --name ${NAME}  airhacks/payara-ping
